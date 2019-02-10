0260b7fde3e03b21e8dd1496c7f801d85a4e85f1
Jayeshpatel1218 <jy331399@dal.ca>
2019-02-10 01:29:07 +0000

    task3

---
+++ l5t3.py
@@ -0,0 +1,9 @@
+import turtle 
+from l5t2 import Rectangle
+from l5t1 import Point
+
+def draw_rectangle(rect):
+  turtle.up()
+  turtle.down()
+  turtle.forward(rect.width)          
+  turtle.left(90)
