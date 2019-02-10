296685bba18041b7138e71464e82446fabe0f59f
Jayeshpatel1218 <47113191+Jayeshpatel1218@users.noreply.github.com>
2019-02-09 20:34:44 -0500

    Delete l5t3.py

--- l5t3.py
+++
@@ -1,9 +0,0 @@
-import turtle 
-from l5t2 import Rectangle
-from l5t1 import Point
-
-def draw_rectangle(rect):
-  turtle.up()
-  turtle.down()
-  turtle.forward(rect.width)          
-  turtle.left(90)
