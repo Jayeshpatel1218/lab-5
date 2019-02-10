8ffdb256d027c1face5284321a85c64374253725
Jayeshpatel1218 <jy331399@dal.ca>
2019-02-10 02:13:35 +0000

    solution for task3

--- l5t3.py
+++ l5t3.py
@@ -1,8 +1,19 @@
-from time import gmtime, strftime
+class Time:
+	def __init__(self):
+		self.hour = 0
+		self.minute = 0
+		self.second = 0
+	
 
-def print_time():
-    print("Current Time is:",strftime("%H:%M:%S", gmtime()))
+def print_time(time):
+	print("%.2d:%.2d:%.2d" %(time.hour,time.minute,time.second))
 
-print_time()    
 
+
+
+time = Time()
+time.hour = 13
+time.minute = 39
+time.second = 15
+print_time(time)
 
